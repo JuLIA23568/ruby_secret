494ee0fba0635aba74e34621994e6b39188e6e51
Julia Ponteley <ponteleju@gmail.com>
2018-07-16 21:50:02 +0300

    Add seeds for companies, documents and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+tesla = Company.create! name: 'Tesla', domain: 'tesla.com'
+google = Company.create! name: 'Google', domain: 'google.com'
+amazon = Company.create! name: 'Amazon', domain: 'amazon.com'
+
+
+#loop through each company
+[tesla, google, amazon].each do |company|
+	#create a user for each company
+	['julia', 'niki','olga'].each do |username|
+		email = "#{username}@#{company.domain}"
+		User.create!(email:email, password: 'password')
+	end
+
+	#create top secret documents that only company can acess 
+	name = 'article 2018'
+	content = "#{company.name}'s article for 2018"
+	Document.create!(company:company, name:name, content: content)
+
+
+	name = 'just second article'
+	technologies = ['At','BV', 'Streaming video']
+	mediums = ['in space', 'on a remote', 'for dog']
+	big_thing = "#{technologies.sample}#{mediums.sample}" 
+	content = "the second article is something only #{company.name} can do #{big_thing}."
+	Document.create!(company:company, name:name, content:content)
+
+
+
+
+end
